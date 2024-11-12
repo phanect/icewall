cdd794163fb04ab1d9ee087ff5d5756bd6a7f487
Jumpei Ogawa <git@phanective.org>
2024-11-12 17:49:24 +0900

    refactor: merge HTML file as JSX so that it works on Cloudflare Workers

--- workspaces/backend/src/routes/index.tsx
+++ workspaces/backend/src/routes/index.tsx
@@ -1,4 +1,3 @@
-import { readFile } from "node:fs/promises";
 import { Hono } from "hono";
 import { githubLoginRouter } from "./github.ts";
 
@@ -13,6 +12,17 @@ loginRouter.get("/login", async (c) => {
   if (session) {
     return c.redirect("/");
   }
-  const htmlFile = await readFile("routes/login/index.html");
-  return c.html(htmlFile.toString(), 200);
+  return c.html((
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <title>Lucia example</title>
+      </head>
+      <body>
+        <h1>Sign in</h1>
+        <a href="/login/github">Sign in with GitHub</a>
+      </body>
+    </html>
+  ), 200);
 });
