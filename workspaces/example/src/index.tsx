cdef9cf72632f5eef357d7657d18e04d8508ade7
Jumpei Ogawa <git@phanective.org>
2024-11-13 14:26:09 +0900

    refactor: chain routers

--- workspaces/example/src/index.tsx
+++ workspaces/example/src/index.tsx
@@ -2,28 +2,29 @@ import { Hono } from "hono";
 
 import type { Env } from "../lib/types.ts";
 
-export const mainRouter = new Hono<Env>();
+const app = new Hono<Env>()
+  .get("/", async (c) => {
+    const user = c.get("user");
+    if (!user) {
+      return c.redirect("/login");
+    }
 
-mainRouter.get("/", async (c) => {
-  const user = c.get("user");
-  if (!user) {
-    return c.redirect("/login");
-  }
+    return c.html((
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width" />
+          <title>Login status</title>
+        </head>
+        <body>
+          <h1>Hi, { user.username }!</h1>
+          <p>Your user ID is { user.id }.</p>
+          <form method="post">
+            <button>Sign out</button>
+          </form>
+        </body>
+      </html>
+    ), 200);
+  });
 
-  return c.html((
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width" />
-        <title>Lucia example</title>
-      </head>
-      <body>
-        <h1>Hi, { user.username }!</h1>
-        <p>Your user ID is { user.id }.</p>
-        <form method="post">
-          <button>Sign out</button>
-        </form>
-      </body>
-    </html>
-  ), 200);
-});
+export default app;
