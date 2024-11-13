c4c61dff89808b649a923a7979b14e270a349501
Jumpei Ogawa <git@phanective.org>
2024-11-13 14:34:26 +0900

    refactor: merge logout router & middlewares to routes/index.tsx

--- workspaces/backend/src/index.ts
+++ workspaces/backend/src/index.ts
@@ -1,43 +1,9 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
-import { verifyRequestOrigin } from "./libs/request.ts";
-import { getLuciaInstance } from "./libs/auth.ts";
-import { logoutRouter } from "./routes/logout.ts";
-import { loginRouter } from "./routes/index.tsx";
 import type { Env } from "./libs/types.ts";
 
 const app = new Hono<Env>()
-  .use("*", async (c, next) => {
-    if (c.req.method === "GET") {
-      return next();
-    }
-    const originHeader = c.req.header("Origin") ?? null;
-    const hostHeader = c.req.header("Host") ?? null;
-    if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [ hostHeader ])) {
-      return c.body(null, 403);
-    }
-    return next();
-  }).use("*", async (c, next) => {
-    const lucia = getLuciaInstance(c);
-    const sessionId = lucia.readSessionCookie(c.req.header("Cookie") ?? "");
-    if (!sessionId) {
-      c.set("user", null);
-      c.set("session", null);
-      return next();
-    }
-
-    const { session, user } = await lucia.validateSession(sessionId);
-    if (session?.fresh) {
-      c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
-    }
-    if (!session) {
-      c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), { append: true });
-    }
-    c.set("user", user);
-    c.set("session", session);
-    return next();
-  }).route("/", loginRouter)
-  .route("/", logoutRouter);
+  .route("/", routers);
 
 serve({
   fetch: app.fetch,
--- workspaces/backend/src/routes/index.tsx
+++ workspaces/backend/src/routes/index.tsx
@@ -1,9 +1,41 @@
 import { Hono } from "hono";
+import { getLuciaInstance } from "./internal/auth.ts";
+import { verifyRequestOrigin } from "./internal/lucia/index.ts";
 import { githubLoginRouter } from "./github.ts";
 
 import type { Env } from "../libs/types.ts";
 
-export const loginRouter = new Hono<Env>()
+export const authRoutes = new Hono<Env>()
+  .use("*", async (c, next) => {
+    if (c.req.method === "GET") {
+      return next();
+    }
+    const originHeader = c.req.header("Origin") ?? null;
+    const hostHeader = c.req.header("Host") ?? null;
+    if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [ hostHeader ])) {
+      return c.body(null, 403);
+    }
+    return next();
+  }).use("*", async (c, next) => {
+    const lucia = getLuciaInstance(c);
+    const sessionId = lucia.readSessionCookie(c.req.header("Cookie") ?? "");
+    if (!sessionId) {
+      c.set("user", null);
+      c.set("session", null);
+      return next();
+    }
+
+    const { session, user } = await lucia.validateSession(sessionId);
+    if (session?.fresh) {
+      c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
+    }
+    if (!session) {
+      c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), { append: true });
+    }
+    c.set("user", user);
+    c.set("session", session);
+    return next();
+  })
   .route("/", githubLoginRouter)
   .get("/login", async (c) => {
     const session = c.get("session");
@@ -23,4 +55,13 @@ export const loginRouter = new Hono<Env>()
         </body>
       </html>
     ), 200);
+  }).post("/", async (c) => {
+    const lucia = getLuciaInstance(c);
+    const session = c.get("session");
+    if (!session) {
+      return c.body(null, 401);
+    }
+    await lucia.invalidateSession(session.id);
+    c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), { append: true });
+    return c.redirect("/login");
   });
