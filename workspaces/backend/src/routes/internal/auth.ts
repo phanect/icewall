f534369053f43a6a9adecc233c892c337e60e96f
Jumpei Ogawa <git@phanective.org>
2024-11-13 18:42:34 +0900

    feat: do not call dotenv from the library

--- workspaces/backend/src/routes/internal/auth.ts
+++ workspaces/backend/src/routes/internal/auth.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import dotenv from "dotenv";
 import { Lucia } from "./lucia/index.ts";
 import { PrismaAdapter } from "./lucia/prisma-adapter.ts";
 import { isLocal } from "./utils.ts";
 import type { Context } from "hono";
 import type { Env } from "../../types.ts";
-
-dotenv.config();
 
 export const prisma = new PrismaClient();
 
