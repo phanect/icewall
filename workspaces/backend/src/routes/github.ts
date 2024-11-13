d327f065010a29330368ca391f25be142aa6ecf3
Jumpei Ogawa <git@phanective.org>
2024-11-13 14:43:00 +0900

    refactor: sort & rename GitHub router

--- workspaces/backend/src/routes/github.ts
+++ workspaces/backend/src/routes/github.ts
@@ -12,7 +12,7 @@ type GitHubUser = {
   login: string;
 };
 
-export const githubLoginRouter = new Hono<Env>()
+export const githubRouter = new Hono<Env>()
   .get("/login/github", async (c) => {
     const {
       SERVER_ENV,
