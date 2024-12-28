f697e61633bfb06db6674ee2f1f63c7aa2f45702
Jumpei Ogawa <git@phanective.org>
2024-12-28 18:40:59 +0900

    feat: do not run Node.js server but export as a library

--- workspaces/icedgate/src/index.ts
+++ workspaces/icedgate/src/index.ts
@@ -1,9 +1,1 @@
-import { serve } from "@hono/node-server";
-import { icedgate } from "./routes/index.ts";
-
-serve({
-  fetch: icedgate.fetch,
-  port: 3000,
-});
-
-console.log("Server running on port 3000");
+export { icedgate } from "./routes/index.ts";
