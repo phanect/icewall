5d2b88bc988754222dd9ef33665c7fe39e3bf409
Jumpei Ogawa <git@phanective.org>
2025-01-02 15:12:08 +0900

    feat: support PostgreSQL & MySQL

---
+++ workspaces/icedgate/src/db/dbms.sqlite.ts
@@ -0,0 +1,10 @@
+import { integer } from "drizzle-orm/sqlite-core";
+
+export {
+  sqliteTable as table,
+  text,
+  integer,
+} from "drizzle-orm/sqlite-core";
+
+export const boolean = (): ReturnType<typeof integer<"boolean">> => integer({ mode: "boolean" });
+export const timestamp = (): ReturnType<typeof integer<"timestamp">> => integer({ mode: "timestamp" });
--- workspaces/icedgate/src/db/schema/session.ts
+++ workspaces/icedgate/src/db/schema/session.ts
@@ -1,12 +1,11 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { table, boolean, text, timestamp } from "../dbms.ts";
 import { IcedGateUsersTable } from "./user.ts";
 import type { InferSelectModel } from "drizzle-orm";
 
-export const IcedGateSessionsTable = sqliteTable("IcedGateSessions", {
+export const IcedGateSessionsTable = table("IcedGateSessions", {
   id: text().primaryKey(),
-  fresh: integer({ mode: "boolean" })
-    .notNull(),
-  expiresAt: integer({ mode: "timestamp" }).notNull(),
+  fresh: boolean().notNull(),
+  expiresAt: timestamp().notNull(),
   userId: text()
     .notNull()
     .references(() => IcedGateUsersTable.id),
