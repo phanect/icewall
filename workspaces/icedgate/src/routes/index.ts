7cecd966862d8c1b5482966ab1e2862d118012df
Jumpei Ogawa <git@phanective.org>
2025-01-06 06:18:23 +0900

    fix: create middleware using `createMiddleware()`
    to fix bug that user object is not passed to the application which use IcedGate.

--- workspaces/icedgate/src/routes/index.ts
+++ workspaces/icedgate/src/routes/index.ts
@@ -6,6 +6,6 @@ import { github } from "./github.ts";
 import type { IcedGateEnv } from "../types.ts";
 
 export const icedgate = new Hono<IcedGateEnv>()
-  .route("*", middleware)
+  .use(middleware)
   .route("/", ui)
   .route("/", github);
