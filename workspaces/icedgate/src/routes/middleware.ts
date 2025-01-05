9c8c70d09c60ef16529a3bb2b0120af598969b5b
Jumpei Ogawa <git@phanective.org>
2025-01-06 06:18:23 +0900

    fix: create middleware using `createMiddleware()`
    to fix bug that user object is not passed to the application which use IcedGate.

--- workspaces/icedgate/src/routes/middleware.ts
+++ workspaces/icedgate/src/routes/middleware.ts
@@ -1,4 +1,4 @@
-import { Hono } from "hono";
+import { createMiddleware } from "hono/factory";
 import { drizzle } from "drizzle-orm/d1";
 import { Lucia } from "../libs/core.ts";
 import { DrizzleSQLiteAdapter } from "../libs/adapter-sqlite.ts";
@@ -6,54 +6,53 @@ import { verifyRequestOrigin } from "../libs/request.ts";
 import { isLocal } from "../libs/utils.ts";
 import type { IcedGateEnv } from "../types.ts";
 
-export const middleware = new Hono<IcedGateEnv>()
-  .use("*", async (c, next) => {
-    if (c.req.method !== "GET") {
-      const originHeader = c.req.header("Origin");
-      const hostHeader = c.req.header("Host");
-      if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [ hostHeader ])) {
-        return c.body(null, 403);
-      }
+export const middleware = createMiddleware<IcedGateEnv>(async (c, next) => {
+  if (c.req.method !== "GET") {
+    const originHeader = c.req.header("Origin");
+    const hostHeader = c.req.header("Host");
+    if (!originHeader || !hostHeader || !verifyRequestOrigin(originHeader, [ hostHeader ])) {
+      return c.body(null, 403);
     }
+  }
 
-    if (!c.env.D1) {
-      throw new Error(`\`D1\` is not set as a Cloudflare D1 binding.
+  if (!c.env.D1) {
+    throw new Error(`\`D1\` is not set as a Cloudflare D1 binding.
 Set the following config in wrangler.toml.
 
 [[d1_databases]]
 binding: "D1"
 `);
-    }
+  }
 
-    const db = drizzle(c.env.D1);
-    const lucia = new Lucia(
-      new DrizzleSQLiteAdapter(db),
-      {
-        sessionCookie: {
-          attributes: {
-            secure: !isLocal(c),
-          },
+  const db = drizzle(c.env.D1);
+  const lucia = new Lucia(
+    new DrizzleSQLiteAdapter(db),
+    {
+      sessionCookie: {
+        attributes: {
+          secure: !isLocal(c),
         },
-      }
-    );
-    c.set("drizzle", db);
-    c.set("lucia", lucia);
+      },
+    }
+  );
+  c.set("drizzle", db);
+  c.set("lucia", lucia);
 
-    const sessionId = lucia.readSessionCookie(c.req.header("Cookie") ?? "");
-    if (!sessionId) {
-      c.set("user", undefined);
-      c.set("session", undefined);
-      return next();
-    }
-
-    const { session, user } = await lucia.validateSession(sessionId);
-    if (session?.fresh) {
-      c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
-    }
-    if (!session) {
-      c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), { append: true });
-    }
-    c.set("user", user);
-    c.set("session", session);
+  const sessionId = lucia.readSessionCookie(c.req.header("Cookie") ?? "");
+  if (!sessionId) {
+    c.set("user", undefined);
+    c.set("session", undefined);
     return next();
-  });
+  }
+
+  const { session, user } = await lucia.validateSession(sessionId);
+  if (session?.fresh) {
+    c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
+  }
+  if (!session) {
+    c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), { append: true });
+  }
+  c.set("user", user);
+  c.set("session", session);
+  return next();
+});
