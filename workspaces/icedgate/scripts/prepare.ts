87026943072d094462b49cf3831253220a40e170
Jumpei Ogawa <git@phanective.org>
2025-01-06 06:28:03 +0900

    refactor: remove unnecessary drizzle.config.ts
    For now, IcedGate only support Cloudflare D1 + SQLite and Drizzle.

--- workspaces/icedgate/scripts/prepare.ts
+++ workspaces/icedgate/scripts/prepare.ts
@@ -1,17 +1,11 @@
 import { copyFile, rm } from "node:fs/promises";
 import { join } from "node:path";
-import drizzleConfig from "../drizzle.config.ts";
 
-if (
-  drizzleConfig.dialect !== "sqlite"
-  && drizzleConfig.dialect !== "postgresql"
-  && drizzleConfig.dialect !== "mysql"
-) {
-  throw new Error(`Unsupported dialect ${ drizzleConfig.dialect } configured in drizzle.config.ts.`);
-}
+// TODO read dialect from user configuration
+const dialect: "sqlite" | "postgresql" | "mysql" = "sqlite";
 
 await rm(join(import.meta.dirname, "../src/db/dbms.ts"), { force: true });
 await copyFile(
-  join(import.meta.dirname, `../src/db/dbms.${ drizzleConfig.dialect }.ts`),
+  join(import.meta.dirname, `../src/db/dbms.${ dialect }.ts`),
   join(import.meta.dirname, "../src/db/dbms.ts"),
 );
