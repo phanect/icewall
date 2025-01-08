f953c4cc1d0bfb830ab82ec7899fdc779ab079ee
Jumpei Ogawa <git@phanective.org>
2025-01-08 19:20:40 +0900

    feat: replace OAuth icons from font-awsome with FeatherIcons'

--- workspaces/icedgate/scripts/prepare.ts
+++ workspaces/icedgate/scripts/prepare.ts
@@ -1,11 +1,53 @@
-import { copyFile, rm } from "node:fs/promises";
+import { copyFile, mkdir, readFile, rm, writeFile } from "node:fs/promises";
 import { join } from "node:path";
+import { optimize as optimizeSVG } from "svgo";
 
-// TODO read dialect from user configuration
-const dialect: "sqlite" | "postgresql" | "mysql" = "sqlite";
+const copyDbmsTs = async () => {
+  // TODO read dialect from user configuration
+  const dialect: "sqlite" | "postgresql" | "mysql" = "sqlite";
 
-await rm(join(import.meta.dirname, "../src/db/dbms.ts"), { force: true });
-await copyFile(
-  join(import.meta.dirname, `../src/db/dbms.${ dialect }.ts`),
-  join(import.meta.dirname, "../src/db/dbms.ts"),
-);
+  await rm(join(import.meta.dirname, "../src/db/dbms.ts"), { force: true });
+  await copyFile(
+    join(import.meta.dirname, `../src/db/dbms.${ dialect }.ts`),
+    join(import.meta.dirname, "../src/db/dbms.ts"),
+  );
+};
+
+const copyIcons = async () => {
+  const services = [
+    "Google",
+    "Facebook",
+    "GitHub",
+  ];
+
+  const featherIconTargetDirPath = join(import.meta.dirname, "../src/vendor/feathericon/");
+
+  await rm(featherIconTargetDirPath, {
+    force: true,
+    recursive: true,
+  });
+  await mkdir(featherIconTargetDirPath, { recursive: true });
+
+  await Promise.all(
+    services.map(async (service) => {
+      const icon = service.toLowerCase();
+      const svgPath = join(import.meta.dirname, `../node_modules/feathericon/build/svg/${ icon }.svg`);
+
+      const svgContent = (await readFile(svgPath)).toString();
+      const { data: optimizedSvgContent } = optimizeSVG(svgContent, { path: svgPath });
+
+      await writeFile(
+        join(featherIconTargetDirPath, `${ icon }.tsx`),
+        `
+          import type { FC } from "hono/jsx";
+          export const ${ service }Icon: FC = () => (${ optimizedSvgContent });
+        `.trim(),
+      );
+    })
+  );
+};
+
+await Promise.all([
+  copyDbmsTs(),
+  copyIcons(),
+]);
