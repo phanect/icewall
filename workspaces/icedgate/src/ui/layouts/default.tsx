916358c8e1d139846374b3fe30663bc5147a1b4e
Jumpei Ogawa <git@phanective.org>
2025-01-08 12:25:43 +0900

    chore: add `initial-scale=1.0` to viewport declaration

--- workspaces/icedgate/src/ui/layouts/default.tsx
+++ workspaces/icedgate/src/ui/layouts/default.tsx
@@ -14,7 +14,7 @@ export const Layout: FC<LayoutProps> = ({
   <html lang={lang}>
     <head>
       <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>{ title }</title>
     </head>
 
