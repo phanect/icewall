4e0d406a9da7e0363db44ca3f88376d90b941d85
Jumpei Ogawa <git@phanective.org>
2025-01-12 06:21:21 +0900

    refactor: rewrite `position` with flexbox

--- workspaces/icedgate/src/ui/styles/layout.style.ts
+++ workspaces/icedgate/src/ui/styles/layout.style.ts
@@ -32,10 +32,12 @@ export const styles = css`
     }
 
     .container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
       background-color: #fff;
       border-radius: 30px;
       box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.35);
-      position: relative;
       overflow: hidden;
       width: 768px;
       max-width: 100%;
@@ -100,7 +102,6 @@ export const styles = css`
     }
 
     .form-container{
-      position: absolute;
       top: 0;
       height: 100%;
       transition: all 0.6s ease-in-out;
@@ -169,15 +170,11 @@ export const styles = css`
     }
 
     .toggle-container{
-      position: absolute;
-      top: 0;
-      left: 50%;
       width: 50%;
       height: 100%;
       overflow: hidden;
       transition: all 0.6s ease-in-out;
       border-radius: 150px 0 0 100px;
-      z-index: 1000;
     }
 
     .container.active .toggle-container{
@@ -188,8 +185,6 @@ export const styles = css`
     .toggle{
       height: 100%;
       background: orangered;
-      position: relative;
-      left: -100%;
       height: 100%;
       width: 200%;
       transform: translateX(0);
@@ -201,7 +196,6 @@ export const styles = css`
     }
 
     .toggle-panel{
-      position: absolute;
       color: #fff;
       width: 50%;
       height: 100%;
@@ -211,7 +205,6 @@ export const styles = css`
       flex-direction: column;
       padding: 0 30px;
       text-align: center;
-      top: 0;
       transform: translateX(0);
       transition: all 0.6s ease-in-out;
     }
@@ -225,7 +218,6 @@ export const styles = css`
     }
 
     .toggle-right{
-      right: 0;
       transform: translateX(0);
     }
 
