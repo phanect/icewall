phanect/authserver

refactor: fix paths by renaming paths

workspaces/backend/src/libs/types.ts
@@ -1,5 +1,5 @@
 import type { Env } from "hono";
-import type { User, Session } from "lucia";
+import type { User, Session } from "./lucia/index.ts";
 
 export type Context = {
   Variables: {

phanect/icewall

workspaces/icedgate/src/types.ts
@@ -1,5 +1,5 @@
 import type { Env } from "hono";
-import type { User, Session } from "lucia";
+import type { User, Session } from "./libs/index.ts";
 
 export type Context = {
   Variables: {