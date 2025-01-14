phanect/authserver

feat: export schema so that the users can use them with one line code

workspaces/icedgate/src/schema.ts
@@ -1,0 +1,2 @@
+export { IcedGateUsers } from "./db/schema/user.ts";
+export { IcedGateSessions } from "./db/schema/session.ts";

phanect/icewall

workspaces/icedgate/src/schema.ts
@@ -1,0 +1,2 @@
+export { IcedGateUsersTable } from "./db/schema/user.ts";
+export { IcedGateSessionsTable } from "./db/schema/session.ts";