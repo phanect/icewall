phanect/authserver

refactor: use Drizzle as an SQL client library

workspaces/icedgate/src/db/schema/session.ts
@@ -1,0 +1,15 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { IcedGateUsers } from "./user.ts";
+import type { InferSelectModel } from "drizzle-orm";
+
+export const IcedGateSessions = sqliteTable("IcedGateSessions", {
+  id: text().primaryKey(),
+  fresh: integer({ mode: "boolean" })
+    .notNull(),
+  expiresAt: integer({ mode: "timestamp" }).notNull(),
+  userId: text()
+    .notNull()
+    .references(() => IcedGateUsers.id),
+});
+
+export type IcedGateSession = InferSelectModel<typeof IcedGateSessions>;

workspaces/icedgate/src/db/schema/user.ts
@@ -1,0 +1,10 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import type { InferSelectModel } from "drizzle-orm";
+
+export const IcedGateUsers = sqliteTable("IcedGateUsers", {
+  id: text().primaryKey(),
+  username: text().unique().notNull(),
+  githubId: int().unique(),
+});
+
+export type IcedGateUser = InferSelectModel<typeof IcedGateUsers>;

workspaces/icedgate/src/libs/auth.ts
@@ -1,27 +1,28 @@
-import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
 import { GitHub } from "arctic";
+import { drizzle } from "drizzle-orm/better-sqlite3";
 import dotenv from "dotenv";
-import { db, type DatabaseUser } from "./db.ts";
+import { type DatabaseUser } from "./db.ts";
 import { Lucia } from "./index.ts";
+import { DrizzleSQLiteAdapter } from "./adapter-sqlite.ts";
+import { IcedGateUsers } from "../db/schema/user.ts";
+import { IcedGateSessions } from "../db/schema/session.ts";
 
 dotenv.config();
 
-const adapter = new BetterSqlite3Adapter(db, {
-  user: "user",
-  session: "session",
-});
-
-export const lucia = new Lucia(adapter, {
-  sessionCookie: {
-    attributes: {
-      secure: process.env.NODE_ENV === "production",
+export const lucia = new Lucia(
+  new DrizzleSQLiteAdapter(drizzle(), IcedGateSessions, IcedGateUsers),
+  {
+    sessionCookie: {
+      attributes: {
+        secure: process.env.NODE_ENV === "production",
+      },
     },
-  },
-  getUserAttributes: (attributes) => ({
-    githubId: attributes.github_id,
-    username: attributes.username,
-  }),
-});
+    getUserAttributes: (attributes) => ({
+      githubId: attributes.github_id,
+      username: attributes.username,
+    }),
+  }
+);
 
 export const github = new GitHub(
   process.env.GITHUB_CLIENT_ID!,

workspaces/icedgate/src/routes/github.ts
@@ -1,9 +1,10 @@
 import { OAuth2RequestError, generateState } from "arctic";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { getCookie, setCookie } from "hono/cookie";
-import { generateId } from "../libs/index.ts";
+import { IcedGateUsers } from "../db/schema/user.ts";
+import { generateId } from "../libs/crypto.ts";
 import { github, lucia } from "../libs/auth.ts";
-import { db, type DatabaseUser } from "../libs/db.ts";
 import type { Context } from "../libs/types.ts";
 
 type GitHubUser = {
@@ -41,9 +42,10 @@ githubLoginRouter.get("/login/github/callback", async (c) => {
       },
     });
     const githubUser: GitHubUser = await githubUserResponse.json();
-    const existingUser: DatabaseUser | undefined = (db
-      .prepare("SELECT * FROM user WHERE github_id = ?")
-      .get(githubUser.id) ?? undefined) as DatabaseUser | undefined;
+    const [ existingUser ] = await drizzle.select().from(IcedGateUsers)
+      .limit(1)
+      .where(eq(IcedGateUsers.githubId, githubUser.id));
+
     if (existingUser) {
       const session = await lucia.createSession(existingUser.id, {});
       c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
@@ -51,11 +53,12 @@ githubLoginRouter.get("/login/github/callback", async (c) => {
     }
 
     const userId = generateId(15);
-    db.prepare("INSERT INTO user (id, github_id, username) VALUES (?, ?, ?)").run(
-      userId,
-      githubUser.id,
-      githubUser.login
-    );
+    await drizzle.insert(IcedGateUsers).values({
+      id: userId,
+      githubId: githubUser.id,
+      username: githubUser.login,
+    });
+
     const session = await lucia.createSession(userId, {});
     c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
     return c.redirect("/");

phanect/icewall

workspaces/icedgate/src/db/schema/session.ts
@@ -1,0 +1,15 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { IcedGateUsersTable } from "./user.ts";
+import type { InferSelectModel } from "drizzle-orm";
+
+export const IcedGateSessionsTable = sqliteTable("IcedGateSessions", {
+  id: text().primaryKey(),
+  fresh: integer({ mode: "boolean" })
+    .notNull(),
+  expiresAt: integer({ mode: "timestamp" }).notNull(),
+  userId: text()
+    .notNull()
+    .references(() => IcedGateUsersTable.id),
+});
+
+export type IcedGateSession = InferSelectModel<typeof IcedGateSessionsTable>;

workspaces/icedgate/src/db/schema/user.ts
@@ -1,0 +1,10 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import type { InferSelectModel } from "drizzle-orm";
+
+export const IcedGateUsersTable = sqliteTable("IcedGateUsers", {
+  id: text().primaryKey(),
+  username: text().unique().notNull(),
+  githubId: int().unique(),
+});
+
+export type IcedGateUser = InferSelectModel<typeof IcedGateUsersTable>;

workspaces/icedgate/src/libs/auth.ts
@@ -1,27 +1,28 @@
-import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
 import { GitHub } from "arctic";
+import { drizzle } from "drizzle-orm/better-sqlite3";
 import dotenv from "dotenv";
-import { db, type DatabaseUser } from "./db.ts";
+import { type DatabaseUser } from "./db.ts";
 import { Lucia } from "./index.ts";
+import { DrizzleSQLiteAdapter } from "./adapter-sqlite.ts";
+import { IcedGateUsersTable } from "../db/schema/user.ts";
+import { IcedGateSessionsTable } from "../db/schema/session.ts";
 
 dotenv.config();
 
-const adapter = new BetterSqlite3Adapter(db, {
-  user: "user",
-  session: "session",
-});
-
-export const lucia = new Lucia(adapter, {
-  sessionCookie: {
-    attributes: {
-      secure: process.env.NODE_ENV === "production",
+export const lucia = new Lucia(
+  new DrizzleSQLiteAdapter(drizzle(), IcedGateSessionsTable, IcedGateUsersTable),
+  {
+    sessionCookie: {
+      attributes: {
+        secure: process.env.NODE_ENV === "production",
+      },
     },
-  },
-  getUserAttributes: (attributes) => ({
-    githubId: attributes.github_id,
-    username: attributes.username,
-  }),
-});
+    getUserAttributes: (attributes) => ({
+      githubId: attributes.github_id,
+      username: attributes.username,
+    }),
+  }
+);
 
 export const github = new GitHub(
   process.env.GITHUB_CLIENT_ID!,

workspaces/icedgate/src/routes/github.ts
@@ -1,9 +1,10 @@
 import { OAuth2RequestError, generateState } from "arctic";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { getCookie, setCookie } from "hono/cookie";
-import { generateId } from "../libs/index.ts";
+import { IcedGateUsersTable } from "../db/schema/user.ts";
+import { generateId } from "../libs/crypto.ts";
 import { github, lucia } from "../libs/auth.ts";
-import { db, type DatabaseUser } from "../libs/db.ts";
 import type { Context } from "../types.ts";
 
 type GitHubUser = {
@@ -41,9 +42,10 @@ githubLoginRouter.get("/login/github/callback", async (c) => {
       },
     });
     const githubUser: GitHubUser = await githubUserResponse.json();
-    const existingUser: DatabaseUser | undefined = (db
-      .prepare("SELECT * FROM user WHERE github_id = ?")
-      .get(githubUser.id) ?? undefined) as DatabaseUser | undefined;
+    const [ existingUser ] = await drizzle.select().from(IcedGateUsersTable)
+      .limit(1)
+      .where(eq(IcedGateUsersTable.githubId, githubUser.id));
+
     if (existingUser) {
       const session = await lucia.createSession(existingUser.id, {});
       c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
@@ -51,11 +53,12 @@ githubLoginRouter.get("/login/github/callback", async (c) => {
     }
 
     const userId = generateId(15);
-    db.prepare("INSERT INTO user (id, github_id, username) VALUES (?, ?, ?)").run(
-      userId,
-      githubUser.id,
-      githubUser.login
-    );
+    await drizzle.insert(IcedGateUsersTable).values({
+      id: userId,
+      githubId: githubUser.id,
+      username: githubUser.login,
+    });
+
     const session = await lucia.createSession(userId, {});
     c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), { append: true });
     return c.redirect("/");