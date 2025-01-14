phanect/authserver

feat: support PostgreSQL & MySQL

workspaces/icedgate/src/db/schema/session.ts
@@ -1,12 +1,11 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { table, boolean, text, timestamp } from "../dbms.ts";
 import { IcedGateUsers } from "./user.ts";
 import type { InferSelectModel } from "drizzle-orm";
 
-export const IcedGateSessions = sqliteTable("IcedGateSessions", {
+export const IcedGateSessions = table("IcedGateSessions", {
   id: text().primaryKey(),
-  fresh: integer({ mode: "boolean" })
-    .notNull(),
-  expiresAt: integer({ mode: "timestamp" }).notNull(),
+  fresh: boolean().notNull(),
+  expiresAt: timestamp().notNull(),
   userId: text()
     .notNull()
     .references(() => IcedGateUsers.id),

workspaces/icedgate/src/db/schema/user.ts
@@ -1,10 +1,10 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, table, text } from "../dbms.ts";
 import type { InferSelectModel } from "drizzle-orm";
 
-export const IcedGateUsers = sqliteTable("IcedGateUsers", {
+export const IcedGateUsers = table("IcedGateUsers", {
   id: text().primaryKey(),
   username: text().unique().notNull(),
-  githubId: int().unique(),
+  githubId: integer().unique(),
 });
 
 export type IcedGateUser = InferSelectModel<typeof IcedGateUsers>;

phanect/icewall

workspaces/icedgate/src/db/schema/session.ts
@@ -1,12 +1,11 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { table, boolean, text, timestamp } from "../dbms.ts";
 import { IcedGateUsersTable } from "./user.ts";
 import type { InferSelectModel } from "drizzle-orm";
 
-export const IcedGateSessionsTable = sqliteTable("IcedGateSessions", {
+export const IcedGateSessionsTable = table("IcedGateSessions", {
   id: text().primaryKey(),
-  fresh: integer({ mode: "boolean" })
-    .notNull(),
-  expiresAt: integer({ mode: "timestamp" }).notNull(),
+  fresh: boolean().notNull(),
+  expiresAt: timestamp().notNull(),
   userId: text()
     .notNull()
     .references(() => IcedGateUsersTable.id),

workspaces/icedgate/src/db/schema/user.ts
@@ -1,10 +1,10 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, table, text } from "../dbms.ts";
 import type { InferSelectModel } from "drizzle-orm";
 
-export const IcedGateUsersTable = sqliteTable("IcedGateUsers", {
+export const IcedGateUsersTable = table("IcedGateUsers", {
   id: text().primaryKey(),
   username: text().unique().notNull(),
-  githubId: int().unique(),
+  githubId: integer().unique(),
 });
 
 export type IcedGateUser = InferSelectModel<typeof IcedGateUsersTable>;