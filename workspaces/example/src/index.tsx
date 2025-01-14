phanect/authserver

chore: call library from example

workspaces/example/src/index.tsx
@@ -1,10 +1,10 @@
 import { Hono } from "hono";
-
-import type { Env } from "../lib/types.ts";
+import { authRoutes, getUser, type Env } from "authserver";
 
 const app = new Hono<Env>()
+  .route("/", authRoutes)
   .get("/", async (c) => {
-    const user = c.get("user");
+    const user = getUser(c);
     if (!user) {
       return c.redirect("/login");
     }

phanect/icewall

workspaces/example/src/index.tsx
@@ -1,12 +1,12 @@
 import { readFile } from "node:fs/promises";
 import { Hono } from "hono";
+import { icedgate, getUser, type IcedGateEnv } from "icedgate";
 
-import type { IcedGateEnv } from "../../icedgate/src/types.ts";
-
-export const mainRouter = new Hono<IcedGateEnv>();
+export const mainRouter = new Hono<IcedGateEnv>()
+  .route("/", icedgate);
 
 mainRouter.get("/", async (c) => {
-  const user = c.get("user");
+  const user = getUser(c);
   if (!user) {
     return c.redirect("/login");
   }