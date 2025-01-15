ea225d909583a9945936f255cf50adc28ff635c4
Jumpei Ogawa <git@phanective.org>
2025-01-15 16:31:55 +0900

    chore: add public index page to example

--- workspaces/example/src/index.tsx
+++ workspaces/example/src/index.tsx
@@ -8,6 +8,21 @@ const app = new Hono<IcewallEnv>()
   // Redirect again to any URL you like here.
   .get("/auth/postlogin", async (c) => c.redirect("/dashboard"))
   .get("/", async (c) => {
+    return c.html((
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Icewall Demo</title>
+        </head>
+        <body>
+          <h1>Icewall Demo</h1>
+          <p>This is a public page. Anyone can view this page without login.</p>
+          <a href="/auth/login">Login</a>
+        </body>
+      </html>
+    ), 200);
+  }).get("/dashboard", async (c) => {
     const user = getUser(c);
 
     return c.html((
@@ -19,7 +34,10 @@ const app = new Hono<IcewallEnv>()
         </head>
         <body>
           <h1>Hi, { user.username }!</h1>
-          <p>Your user ID is { user.id }.</p>
+          <p>
+            This page is protected by Icewall. You need to login to view this page.<br />
+            Your user ID is { user.id }.
+          </p>
           <a href="/auth/logout">Sign out</a>
         </body>
       </html>
