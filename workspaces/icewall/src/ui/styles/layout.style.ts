c2faafd747ad2de807a266b338801ce7c7348a11
Jumpei Ogawa <git@phanective.org>
2025-01-15 13:09:02 +0900

    chore: add TODO comments

--- workspaces/icewall/src/ui/styles/layout.style.ts
+++ workspaces/icewall/src/ui/styles/layout.style.ts
@@ -1,5 +1,6 @@
 import { css } from "hono/css";
 
+// TODO migrate to better format
 export const styles = css`
   :-hono-global {
     @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
