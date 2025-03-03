phanect/authserver

feat: replace `process.env.NODE_ENV` with `env(context).SERVER_ENV`
...to make it work on Cloudflare Workers and other platforms.

workspaces/backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { verifyRequestOrigin } from "./lib/lucia/index.ts";
-import { lucia } from "./lib/auth.ts";
+import { getLuciaInstance } from "./lib/auth.ts";
 import { mainRouter } from "./routes/index.ts";
 import { logoutRouter } from "./routes/logout.ts";
 import { loginRouter } from "./routes/login/index.ts";
@@ -22,6 +22,7 @@ app.use("*", async (c, next) => {
 });
 
 app.use("*", async (c, next) => {
+  const lucia = getLuciaInstance(c);
   const sessionId = lucia.readSessionCookie(c.req.header("Cookie") ?? "");
   if (!sessionId) {
     c.set("user", null);

phanect/icewall

workspaces/backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { verifyRequestOrigin } from "./lib/lucia/index.ts";
-import { lucia } from "./lib/auth.ts";
+import { getLuciaInstance } from "./libs/auth.ts";
 import { logoutRouter } from "./routes/logout.ts";
 import { loginRouter } from "./routes/login/index.ts";
 import type { Env } from "./lib/types.ts";
@@ -21,6 +21,7 @@ app.use("*", async (c, next) => {
 });
 
 app.use("*", async (c, next) => {
+  const lucia = getLuciaInstance(c);
   const sessionId = lucia.readSessionCookie(c.req.header("Cookie") ?? "");
   if (!sessionId) {
     c.set("user", null);