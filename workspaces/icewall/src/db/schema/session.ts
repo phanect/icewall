5687e6bd18f7073e5d1b682923285c102dc30783
Jumpei Ogawa <git@phanective.org>
2025-03-05 15:15:33 +0900

    refactor: rename table names to singular

--- workspaces/icewall/src/db/schema/session.ts
+++ workspaces/icewall/src/db/schema/session.ts
@@ -2,7 +2,7 @@ import { table, boolean, text, timestamp } from "../dbms.ts";
 import { IcewallUsersTable } from "./user.ts";
 import type { InferSelectModel } from "drizzle-orm";
 
-export const IcewallSessionsTable = table("IcewallSessions", {
+export const IcewallSessionsTable = table("IcewallSession", {
   id: text().primaryKey(),
   fresh: boolean().default(true),
   expiresAt: timestamp().notNull(),
--- workspaces/icewall/src/db/schema/user.ts
+++ workspaces/icewall/src/db/schema/user.ts
@@ -1,7 +1,7 @@
 import { integer, table, text } from "../dbms.ts";
 import type { InferSelectModel } from "drizzle-orm";
 
-export const IcewallUsersTable = table("IcewallUsers", {
+export const IcewallUsersTable = table("IcewallUser", {
   id: text().primaryKey(),
   email: text().unique().notNull(),
   githubId: integer().unique(),
